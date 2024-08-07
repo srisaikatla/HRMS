2f7bdc81b2c16a9254809822e386aead6e2d2675
Ashwini Kumari <ashwini.swe@gmail.com>
2024-08-07 19:51:14 +0530

    updated code

--- src/components/hr/payroll/payroll_forms/PayrollForms.jsx
+++ src/components/hr/payroll/payroll_forms/PayrollForms.jsx
@@ -28,15 +28,15 @@ function PayrollForms() {
           className="flex mx-4 flex-row justify-between items-center"
         >
           <div>
-            <span className="flex text-lg font-bold">Employee</span>
+            <span className="flex text-lg font-bold">Hr</span>
             <span className="text-[16px] font-medium">
-              Dashboard /Payslips /Payroll Forms
+             Payroll /Payroll Forms
             </span>
           </div>
-          <div className="w-auto h-[55px] px-4 flex rounded-lg bg-[#ef5f2b]">
+          <div className="w-auto h-[55px] px-4 flex rounded-lg bg-[#0098f1]">
             <button className="flex px-4 justify-center items-center ">
               <FaPlusCircle className="text-white text-1xl mr-2" />
-              <span className="text-white bg-[#E65F2B] font-medium text-lg">
+              <span className="text-white bg-[#0098f1] font-medium text-lg">
                 Add Settlement
               </span>
             </button>
@@ -45,25 +45,25 @@ function PayrollForms() {
 
         <div className="pt-10 mx-3">
           <span className="pl-10 text-lg">Forms</span>
-          <hr className="bg-[#ef5f2b] h-[2px] border-none my-3" />
+          <hr className="bg-[#0098f1] h-[2px] border-none my-3" />
 
           <div className="flex justify-center items-center h-60">
             <label className="block text-lg font-medium mb-2">Tax Form:</label>
-            <select className="px-20 py-4 ml-2 bg-transparent border-2 border-[#ef5f2b] outline-none">
-              <option className="hover:bg-[#ef5f2b] bg-white" value=""></option>
-              <option className="hover:bg-[#ef5f2b]" value="Form 16">
+            <select className="px-20 py-4 ml-2 bg-transparent border-2 border-[#0098f1] outline-none">
+              <option className="hover:bg-[#0098f1] bg-white" value=""></option>
+              <option className="hover:bg-[#0098f1]" value="Form 16">
                 Form 16
               </option>
-              <option className="hover:bg-[#ef5f2b]" value="Form 16A">
+              <option className="hover:bg-[#0098f1]" value="Form 16A">
                 Form 16A
               </option>
-              <option className="hover:bg-[#ef5f2b]" value="Form 10E">
+              <option className="hover:bg-[#0098f1]" value="Form 10E">
                 Form 10E
               </option>
-              <option className="hover:bg-[#ef5f2b]" value="Form 12B">
+              <option className="hover:bg-[#0098f1]" value="Form 12B">
                 Form 12B
               </option>
-              <option className="hover:bg-[#ef5f2b]" value="Form 26AS">
+              <option className="hover:bg-[#0098f1]" value="Form 26AS">
                 Form 26AS
               </option>
               {/* Add more options as needed */}
@@ -73,26 +73,26 @@ function PayrollForms() {
 
         <div
           id="table"
-          className="overflow-x-scroll scrollbar-thin scrollbar-track-white scrollbar-thumb-orange-600 pt-10 mx-4"
+          className="overflow-x-scroll scrollbar-thin scrollbar-track-white scrollbar-thumb-blue-600 pt-10 mx-4"
         >
           <span className="pl-10 text-lg">Downloads</span>
-          <hr className="bg-[#ef5f2b] w-screen h-[2px] border-none my-3 mb-6" />
+          <hr className="bg-[#0098f1] w-screen h-[2px] border-none my-3 mb-6" />
           <table className="min-w-full w-screen overflow-x-scroll text-nowrap ">
             <thead>
               <tr>
-                <th className="px-4 bg-[#E65F2B] border-r border-white text-white py-2">
+                <th className="px-4 bg-[#0098f1] border-r border-white text-white py-2">
                   Form Type
                 </th>
-                <th className="px-4 bg-[#E65F2B] border-r border-white text-white py-2">
+                <th className="px-4 bg-[#0098f1] border-r border-white text-white py-2">
                   Financial Year
                 </th>
-                <th className="px-4 bg-[#E65F2B] border-r border-white text-white py-2">
+                <th className="px-4 bg-[#0098f1] border-r border-white text-white py-2">
                   Created By
                 </th>
-                <th className="px-4 bg-[#E65F2B] border-r border-white text-white py-2">
+                <th className="px-4 bg-[#0098f1] border-r border-white text-white py-2">
                   Created On
                 </th>
-                <th className="px-4 bg-[#E65F2B] border-r border-white text-white py-2">
+                <th className="px-4 bg-[#0098f1] border-r border-white text-white py-2">
                   Action
                 </th>
               </tr>
@@ -101,21 +101,21 @@ function PayrollForms() {
               {data.map((item, index) => (
                 <tr
                   key={index}
-                  className=" text-[#e65f2b] border-b border-[#e65f2b]"
+                  className=" text-[#0098f1] border-b border-[#0098f1]"
                 >
-                  <td className="px-4 py-2 text-center border-r border-[#e65f2b]">
+                  <td className="px-4 py-2 text-center border-r border-[#0098f1]">
                     {item.formtype}
                   </td>
-                  <td className="px-4 py-2 text-center border-r border-[#e65f2b]">
+                  <td className="px-4 py-2 text-center border-r border-[#0098f1]">
                     {item.financialYear}
                   </td>
-                  <td className="px-4 py-2 text-center border-r border-[#e65f2b]">
+                  <td className="px-4 py-2 text-center border-r border-[#0098f1]">
                     {item.createdBy}
                   </td>
-                  <td className="px-4 py-2 text-center border-r border-[#e65f2b]">
+                  <td className="px-4 py-2 text-center border-r border-[#0098f1]">
                     {item.createdOn}
                   </td>
-                  <td className="px-4 py-2 text-center border-r border-[#e65f2b]">
+                  <td className="px-4 py-2 text-center border-r border-[#0098f1]">
                     {item.action}
                   </td>
                 </tr>
