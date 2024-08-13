edaa71f807073ff62e7c18d62f4c77c67bb1347e
NITTAGANESH <nittaganeshkumar@gmail.com>
2024-08-13 14:56:13 +0530

    employee dashboard integration done

--- src/Config/api.js
+++ src/Config/api.js
@@ -3,6 +3,7 @@
 import axios from 'axios';
 const DEPLOYED = 'https://e-commerce-server-production-0873.up.railway.app'
 const LOCALHOST = 'http://13.234.49.187:8080'
+// const LOCALHOST = 'http://localhost:8080'
 
 export const API_BASE_URL = LOCALHOST
 
