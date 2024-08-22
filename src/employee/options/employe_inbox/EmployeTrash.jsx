9ca0d2b767996e2b96568a7e7fd0c75f64d9a6f1
nikhilraj021 <dnraj2001@gmail.com>
2024-08-22 14:18:26 +0530

    inbox updated

--- src/employee/options/employe_inbox/EmployeTrash.jsx
+++ src/employee/options/employe_inbox/EmployeTrash.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const EmployeTrash = () => {
   return (
-    <div className="rounded-r-xl min-w-[850px] bg-[#FCEFE9]">
-      <div className="flex justify-between py-5 mx-10">
+    <div className="rounded-r-xl min-w-[800px] border border-[#E65F2B] bg-[#FCEFE9]">
+      <div className="flex justify-between py-5 mx-5">
         <h1 className="text-xl font-semibold">Trash</h1>
       </div>
     </div>
