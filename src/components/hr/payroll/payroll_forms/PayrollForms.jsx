bb5aa86f37f60a36f1fe8e28c4e8d99cb51d77c5
Ashwini Kumari <ashwini.swe@gmail.com>
2024-09-04 14:00:10 +0530

    updated code for user from admin side and payrollforms from hr side

--- src/components/hr/payroll/payroll_forms/PayrollForms.jsx
+++ src/components/hr/payroll/payroll_forms/PayrollForms.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Select from "react-select";
 import { FaEdit, FaTrash } from "react-icons/fa";
-
+import { IoMdCheckmarkCircleOutline } from "react-icons/io";
 const employees = [
   { value: "Employee 1", label: "Employee 1" },
   { value: "Employee 2", label: "Employee 2" },
@@ -15,6 +15,9 @@ function PayrollForms() {
   const [selectedEmployees, setSelectedEmployees] = useState([]);
   const [tableData, setTableData] = useState([]);
   const [showTable, setShowTable] = useState(false);
+  const [editIndex, setEditIndex] = useState(null);
+  const [editData, setEditData] = useState({});
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
 
   const handleFormChange = (e) => {
     setSelectedForm(e.target.value);
@@ -61,6 +64,28 @@ function PayrollForms() {
     }
   };
 
+  const handleEditClick = (index) => {
+    setEditIndex(index);
+    setEditData(tableData[index]);
+  };
+
+  const handleSaveEdit = () => {
+    const updatedTableData = [...tableData];
+    updatedTableData[editIndex] = editData;
+    setTableData(updatedTableData);
+    setEditIndex(null);
+    setEditData({});
+    setShowSuccessMessage(true);
+    setTimeout(() => {
+      setShowSuccessMessage(false);
+    }, 3000);
+  };
+
+  const handleDeleteClick = (index) => {
+    const updatedTableData = tableData.filter((_, i) => i !== index);
+    setTableData(updatedTableData);
+  };
+
   return (
     <>
       <div className="main p-4 mt-4 min-h-screen">
@@ -69,14 +94,14 @@ function PayrollForms() {
           className="flex  flex-row justify-between items-center"
         >
           <div>
-            <span className="text-[#0098F1] lg:text-lg text-sm font-bold mb-4">
+            <span className="text-[#0098f1] lg:text-lg text-sm font-bold mb-4">
               Payroll / Payroll Forms
             </span>
           </div>
         </div>
 
         <div className="pt-10 mx-3">
-          <span className="pl-10 text-lg">Forms</span>
+          <span className="pl-10 text-lg font-semibold">Forms</span>
           <hr className="bg-[#0098f1] h-[2px] border-none my-3" />
 
           <div className="flex text-sm lg:text-lg flex-col lg:flex-row justify-center items-center h-auto pt-10">
@@ -115,7 +140,7 @@ function PayrollForms() {
               </div>
               <div className="flex mt-4 space-x-4">
                 <button
-                  className="bg-blue-500 text-white px-4 py-2 rounded"
+                  className="bg-[#0098f1] text-white px-4 py-2 rounded"
                   onClick={handleGenerate}
                 >
                   Generate
@@ -134,7 +159,6 @@ function PayrollForms() {
         {showTable && (
           <>
             <div className="pt-4">
-              <span className="pl-10  text-lg  ">Downloads</span>
               <hr className="bg-[#0098f1] w-auto mx-3 h-[2px] border-none my-3 " />
             </div>
 
@@ -187,15 +211,98 @@ function PayrollForms() {
                         {item.createdOn}
                       </td>
                       <td className="px-4 py-2 text-center border-r border-[#0098f1]">
-                        <FaEdit className="inline-block mr-2 cursor-pointer" />
-                        <FaTrash className="inline-block cursor-pointer" />
+                        <FaEdit
+                          className="inline-block text-blue-500 mr-2 cursor-pointer"
+                          onClick={() => handleEditClick(index)}
+                        />
+                        <FaTrash
+                          className="inline-block text-red-600 cursor-pointer"
+                          onClick={() => handleDeleteClick(index)}
+                        />
                       </td>
                     </tr>
                   ))}
                 </tbody>
               </table>
             </div>
+
+            {editIndex !== null && (
+              <div className="fixed inset-0 z-50  flex items-center justify-center bg-black bg-opacity-50">
+                <div className="bg-white text-[#0098f1]  h-80 scrollbar-thin scrollbar-track-white scrollbar-thumb-[#0098f1] overflow-y-scroll p-6 rounded-md shadow-lg">
+                  <h3 className="text-lg text-[#0098f1] font-semibold mb-4">
+                    Edit Form
+                  </h3>
+                  <div className="mb-4">
+                    <label className="block mb-2 font-medium">Form Type</label>
+                    <input
+                      type="text"
+                      className="w-full border-2 border-gray-300 focus:outline-[#0098f1]  p-2 rounded"
+                      value={editData.formtype}
+                      onChange={(e) =>
+                        setEditData({ ...editData, formtype: e.target.value })
+                      }
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label className="block mb-2 font-medium">
+                      Financial Year
+                    </label>
+                    <input
+                      type="text"
+                      className="w-full border-2 border-gray-300 focus:outline-[#0098f1] p-2 rounded"
+                      value={editData.financialYear}
+                      onChange={(e) =>
+                        setEditData({
+                          ...editData,
+                          financialYear: e.target.value,
+                        })
+                      }
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label className="block mb-2 font-medium">
+                      Created For
+                    </label>
+                    <input
+                      type="text"
+                      className="w-full border-2 border-gray-300 focus:outline-[#0098f1]  p-2 rounded"
+                      value={editData.createdFor}
+                      onChange={(e) =>
+                        setEditData({
+                          ...editData,
+                          createdFor: e.target.value,
+                        })
+                      }
+                    />
+                  </div>
+                  <div className="flex justify-end">
+                    <button
+                      className="bg-gray-500 text-white px-4 py-2 rounded mr-2"
+                      onClick={() => setEditIndex(null)}
+                    >
+                      Cancel
+                    </button>
+                    <button
+                      className="bg-[#0098f1] text-white px-4 py-2 rounded"
+                      onClick={handleSaveEdit}
+                    >
+                      Save
+                    </button>
+                  </div>
+                </div>
+              </div>
+            )}
           </>
+        )}
+        {showSuccessMessage && (
+          <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 z-50">
+            <div className="bg-[#0098f1] p-8 rounded-lg text-center text-white">
+              <h2 className="text-xl mb-4">
+                <IoMdCheckmarkCircleOutline className="inline-block text-6xl" />
+              </h2>
+              <span>Settlement Added Successfully!</span>
+            </div>
+          </div>
         )}
       </div>
     </>
